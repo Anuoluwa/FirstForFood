0e467d2027adbf88ec51b7e8f60e3a33df04ed68
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-11 17:20:37 +0100

    [Feature #160406515] Create endpoint for /POST method to make new orders

--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/', (req, res) => res.json('Successful!, Welcome to SwiftFood API v1!'));
 router.get('/orders', Order.GetAllOrders);
 router.get('/orders/:id', Order.GetOneOrder);
+router.post('/orders', Order.AddOrder);
 
 export default router;
