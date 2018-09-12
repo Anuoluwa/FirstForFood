d60eb4091351a0b16107732d0f1398c86ab3a904
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-12 04:52:05 +0100

    [Chore #160439260] Create validation for inputs, for orders /POST method as middleware

---
+++ api/v1/middlewares/orderValidator.js
@@ -0,0 +1,69 @@
+class ValidateInput {
+  /**
+       * Middleware for validation for all users input to question controller .
+       * @param {req} str - The req is the receiver of inputs from client.
+       * @param {res} str - The res is the carries response to user end from server.
+       * @return {res.status()} A response object and emit appropriate errors.
+       */
+
+  static orderInput(req, res, next) {
+    const {
+      foodItem,
+      quantity,
+      price,
+      address,
+    } = req.body;
+    if (!foodItem) {
+      return res.status(400).json({ message: '"foodItem" must be a string!' })
+        .end();
+    }
+    if (foodItem.length < 10) {
+      return res.status(400).json(
+        { message: '"foodItem" must be a string with minimum 20 characters' },
+      )
+        .end();
+    }
+    if (foodItem.length > 21) {
+      return res.status(400).json({ message: '"foodItem" must be a string with maximum 200 characters' })
+        .end();
+    }
+    if (!quantity) {
+      return res.status(400).json('"quantity" must be a string')
+        .end();
+    }
+    if (quantity.length < 10) {
+      return res.status(400).json({ messge: '"quantity" must be a string with minimum 20 characters' })
+        .end();
+    }
+    if (quantity.length > 21) {
+      return res.status(400).json({ message: '"quantity" must be a string with maximum 20 character ' })
+        .end();
+    }
+    if (!price) {
+      return res.status(400).json({ message: '"price" must be a string!' })
+        .end();
+    }
+    if (price.length < 3) {
+      return res.status(400).json({ message: '"price" must be a string with minimum 20 characters ' })
+        .end();
+    }
+    if (price.length > 9) {
+      return res.status(400).json({ message: '"price" must be a string with maximum 200 characters' })
+        .end();
+    }
+    if (!address) {
+      return res.status(400).json({ message: '"address" must be a string' })
+        .end();
+    }
+    if (address.length < 10) {
+      return res.status(400).json({ message: '"address" must be a string with minimum 20 characters' })
+        .end();
+    }
+    if (address.length > 31) {
+      return res.status(400).json({ message: '"address" must be a string with maximum 20 character ' })
+        .end();
+    }
+    next();
+  }
+}
+export default ValidateInput;
--- api/v1/routes/index.js
+++ api/v1/routes/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import Order from '../controllers/orderController';
+import Validator from '../middlewares/orderValidator';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.json('Successful!, Welcome to SwiftFood API v1!'));
 router.get('/orders', Order.getAllOrders);
 router.get('/orders/:id', Order.getOneOrder);
-router.post('/orders', Order.addOrder);
+router.post('/orders', Validator.orderInput, Order.addOrder);
 router.put('/orders/:id', Order.updateOrder);
 router.delete('/orders/:id', Order.cancelOrder);
 
