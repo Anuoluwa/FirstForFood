b83446a9221ec31c1325306c151d205f11297979
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-14 15:47:51 +0100

    [Bug #160515093] Refactor updateOrder in order controller to remove adding order to data logic

--- api/v1/controllers/orderController.js
+++ api/v1/controllers/orderController.js
@@ -41,24 +41,15 @@ export default class Orders {
       const orderId = parseInt(req.params.id, 10);
       const order = await orders.filter(item => item.orderId == orderId)[0];
       if (!order) {
-        orders.push({
-          orderId: orders.length + 1,
-          date: req.body.date,
-          foodItem: req.body.foodItem,
-          quantity: req.body.quantity,
-          price: req.body.price,
-          address: req.body.address,
-        });
-        res.status(201).json({ message: 'order was created successfully', data: orders });
-      } else {
-        const index = orders.indexOf(order);
-        const keys = Object.keys(req.body);
-        keys.forEach((key) => {
-          order[key] = req.body[key];
-        });
-        orders[index] = order;
-        res.status(202).json({ message: 'Order updated successfully!', data: orders[index] });
+        return res.status(404).json({ message: 'Order does not exist!' });
       }
+      const index = orders.indexOf(order);
+      const keys = Object.keys(req.body);
+      keys.forEach((key) => {
+        order[key] = req.body[key];
+      });
+      orders[index] = order;
+      res.status(202).json({ message: 'Order updated successfully!', data: orders[index] });
     } catch (err) {
       res.status(500).json({ message: 'Sorry about that, not available', err });
     }
