e46e1ac9f6f378d58c5e52e0b81ecbdbc7262a0d
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-20 15:09:39 +0100

    [Chore #160641480] Create config folder for database setup

--- api/v2/config/connect.js
+++ api/v2/config/connect.js
@@ -1,6 +1,6 @@
 import pg from 'pg';
 import config from './config';
 
-const pool = (process.env.NODE_ENV === 'test') ? new pg.Pool(config.test) : new pg.Pool(config.database);
+const pool = (process.env.NODE_ENV === 'test') ? new pg.Pool(config.test) : new pg.Pool(config.development);
 
 export default pool;
