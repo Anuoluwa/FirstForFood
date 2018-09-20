6ebed40ad72f2aef57b6dad66af7c16f9b849595
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-21 00:19:57 +0100

    [Feature #160641620] Add morgan as a middleware logger for traffic in app

--- api/v2/index.js
+++ api/v2/index.js
@@ -1,7 +1,9 @@
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 import routes from './routes/index';
 
 export default (app) => {
+  app.use(morgan('combined'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/api/v2', routes);
