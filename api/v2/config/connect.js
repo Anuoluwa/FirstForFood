0b313fb0f1f7ec099a9cbcb940f8e61f013f43bb
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-21 13:09:36 +0100

    [Feature #160641620] Create config settings to run elephantsql

--- api/v2/config/connect.js
+++ api/v2/config/connect.js
@@ -1,6 +1,32 @@
-import pg from 'pg';
-import config from './config';
+// import dotenv from 'dotenv';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-const pool = (process.env.NODE_ENV === 'test') ? new pg.Pool(config.test) : new pg.Pool(config.development);
+dotenv.config();
+
+const connectionString = process.env.DATABASE;
+const pool = new Pool({
+  connectionString,
+
+});
 
 export default pool;
+
+// import pg from 'pg';
+// import config from './config';
+
+// const nodeEnv = process.env.NODE_ENV;
+// let db;
+// switch (nodeEnv) {
+//   case 'production':
+//     db = process.env.DATABASE_URL;
+//     break;
+//   case 'test':
+//     db = config.test;
+//     break;
+//   default:
+//     db = process.env.DATABASE;
+// }
+// console.log(nodeEnv);
+
+// export default new pg.Pool(db);
