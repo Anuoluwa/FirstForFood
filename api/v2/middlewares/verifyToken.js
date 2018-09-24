80d2143eab7ce97d58ae3993ae217afebe1e5f7f
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-24 02:30:25 +0100

    [Feature 160641620] Add middleware to verify token

--- api/v2/middlewares/verifyToken.js
+++ api/v2/middlewares/verifyToken.js
@@ -3,14 +3,21 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const secret = process.env.SECRET_KEY;
+/** @function verifyAdmin
+/* @param {object} req req object
+/* @param {object} res res object
+/* @param {object} next res object
+/* @returns {Object} res status 401 if no token is provided
+/* @returns {Object} status 501 server error
+*/
 
+const secret = process.env.SECRET_KEY;
 const verifyToken = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       return res.status(401).json({
-        status: 'fail',
+        status: 'Unathorized',
         message: 'No Token provided',
       });
     }
@@ -18,9 +25,9 @@ const verifyToken = (req, res, next) => {
     req.userId = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({
+    return res.status(501).json({
       status: 'unauthorized',
-      message: 'you are not authorized to perform this operation',
+      message: 'you are unauthorized for this operation',
     });
   }
   return null;
