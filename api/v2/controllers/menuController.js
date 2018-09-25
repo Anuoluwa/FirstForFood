a175e7f70993b11c2b61f23823e35e0a7008f1dc
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-25 03:33:47 +0100

    [Feature #160642156] Create controller logic for admin to create menu

--- api/v2/controllers/menuController.js
+++ api/v2/controllers/menuController.js
@@ -1,14 +1,46 @@
+import db from '../config/connection';
+import { createMenu, checkFoodName } from '../models/query';
+
 class MenuController {
+  static async createMenu(req, res) {
+    try {
+      const userId = req.userId.id;
+      const {
+        foodName, foodDescr, price,
+      } = req.body;
+      const foodNameExists = await db.query(checkFoodName(foodName, userId));
+      if (foodNameExists.rowCount > 0) {
+        return res.status(409).json({
+          status: 'conflict',
+          message: 'you already created a food with the same name',
+        });
+      }
+      const food = {
+        foodName, foodDescr, price, userId,
+      };
+      const addMenu = await db.query(createMenu(food));
+      if (addMenu.rowCount > 0) {
+        return res.status(201).json({
+          status: 'operation successful',
+          message: 'Menu created successfully',
+          menu: addMenu.rows,
+        });
+      }
+    } catch (error) {
+      console.log({ message: `${error}` });
+      return res.status(500).json({
+        status: 'operation not successful',
+        message: 'Sorry, something went wrong, try again!',
+      });
+    }
+  }
+
   static async getAllMenu(req, res) {
     res.status(200).json({ message: 'get all menu endpoint logic here' });
   }
 
   static async getOneMenu(req, res) {
     res.status(200).json({ message: 'get one specific menu endpoint logic here' });
-  }
-
-  static async createMenu(req, res) {
-    res.status(200).json({ message: 'create menu endpoint logic here' });
   }
 
   static async updateMenu(req, res) {
