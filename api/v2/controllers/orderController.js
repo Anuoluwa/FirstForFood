3f286e0d41f4fa6a57d860812c8623a2a506e4f0
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-26 13:44:46 +0100

    [Feature #160642126] Initial commit

--- api/v2/controllers/orderController.js
+++ api/v2/controllers/orderController.js
@@ -1,14 +1,16 @@
+import db from '../config/connection';
+import { checkMenuId } from '../models/query';
+
 class OrderController {
+  static async createOrder(req, res) {
+  }
+
   static async getAllOrder(req, res) {
     res.status(200).json({ message: 'get all order endpoint logic here' });
   }
 
   static async getOneOrder(req, res) {
     res.status(200).json({ message: 'get one specific order endpoint logic here' });
-  }
-
-  static async createOrder(req, res) {
-    res.status(200).json({ message: 'create order endpoint logic here' });
   }
 
   static async updateOrder(req, res) {
