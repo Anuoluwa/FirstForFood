3d4be597e67a8e7356aa0a8096dff149f7a3ea98
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-30 05:28:38 +0100

    [Feature #160871718] Add refactor test for order methods controller

--- api/v2/tests/controllers/orderController.spec.js
+++ api/v2/tests/controllers/orderController.spec.js
@@ -4,48 +4,138 @@ import app from '../../../../server';
 
 const { expect } = chai;
 
+const faketoken = 'qwertyuioplkjjdhdhhdhdhhd';
+let userToken;
+
+before((done) => {
+  request(app)
+    .post('/api/v2/auth/signup')
+    .send({
+      username: 'johnpeter',
+      email: 'johnpeter@gmail.com',
+      password: 'johnjane',
+      phone: '07012345678',
+      address: 'qwert asdf',
+    })
+    .end((err, res) => {
+      userToken = res.body.data.token;
+      done();
+    });
+});
 describe('Test suite for orders controller', () => {
   describe(' POST /api/v2/orders', () => {
+    it('should return error for undefined header and token', (done) => {
+      request(app)
+        .post('/api/v2/menu')
+        .send({
+          qty: '12',
+          menuId: '1',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('Headers key: "Authorization" and "token XXXXXXXXX" should be valid');
+          done();
+        });
+    });
+    it('should return reject fake token', (done) => {
+      request(app)
+        .post('/api/v2/menu')
+        .set({ Authorization: `token ${faketoken}` })
+        .send({
+          qty: '12',
+          menuId: '1',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(401);
+          expect(res.body.auth).to.equal('unauthorized');
+          expect(res.body.message).to.equal('Failed to authenticate token');
+          done();
+        });
+    });
+    it('should return error for empty token', (done) => {
+      request(app)
+        .post('/api/v2/menu')
+        .set({ Authorization: '' })
+        .send({
+          qty: '10',
+          menuId: '1',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.status).to.equal('operation not successful');
+          expect(res.body.message).to.equal('Headers key: "Authorization" and "token XXXXXXXXX" should not be empty');
+          done();
+        });
+    });
+    it('should return error for undefined input for new user', (done) => {
+      request(app)
+        .post('/api/v2/orders')
+        .set({ Authorization: `token ${userToken}` })
+        .send({
+          qty: undefined,
+          menuId: '1',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('"qty" must not be undefined!');
+          done();
+        });
+    });
+    it('should return error for undefined input for new order', (done) => {
+      request(app)
+        .post('/api/v2/orders')
+        .set({ Authorization: `token ${userToken}` })
+        .send({
+          qty: '12',
+          menuId: undefined,
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('"menuId" must not be undefined!');
+          done();
+        });
+    });
+    it('should return error for undefined input for new order', (done) => {
+      request(app)
+        .post('/api/v2/orders')
+        .set({ Authorization: `token ${userToken}` })
+        .send({
+          qty: '',
+          menuId: '1',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('"qty" must be a string of minimum of 6 digits');
+          done();
+        });
+    });
+    it('should return error for undefined input for new order', (done) => {
+      request(app)
+        .post('/api/v2/orders')
+        .set({ Authorization: `token ${userToken}` })
+        .send({
+          qty: '123',
+          menuId: '',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('"menuId" must be a string of numbers, no negative numbers');
+          done();
+        });
+    });
     it('should return succcess status code when a orders is created', (done) => {
       request(app)
         .post('/api/v2/orders/')
-        .send({
-          qty: '1',
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .end((err) => {
-          expect(201);
-          if (err) return done(err);
-          done();
-        });
-    });
-    it('should be an object with keys and values', (done) => {
-      request(app)
-        .post('/api/v1/orders/')
-        .set('Accept', 'application/json')
-        .expect(200)
+        .set({ Authorization: `token ${userToken}` })
         .send({
           qty: '23',
+          menuId: '1',
         })
         .end((err, res) => {
+          expect(res.status).to.eql(201);
+          expect(res.body.status).to.equal('success');
+          expect(res.body.message).to.equal('Order have been taken!');
           expect(res.err).to.be.not.eql(null);
-          expect(res.status).to.be.not.eql(null);
-          done();
-        });
-    });
-    it('it return object in json', (done) => {
-      request(app)
-        .post('/api/v1/orders')
-        .send({
-          qty: '1',
-        })
-        .set('Accept', 'application/json')
-        .expect(200)
-        .end((err, res) => {
-          expect(res.body.message).to.not.equal(null);
-          expect(res.body.status).to.not.equal(null);
-          expect(res.body.orders).to.not.equal(null);
           done();
         });
     });
