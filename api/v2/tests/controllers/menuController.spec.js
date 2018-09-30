1af43731d1c5b3e8b456ccb47c66300089147c57
Anuoluwa Apiti <mercies101@gmail.com>
2018-09-30 05:28:08 +0100

    [Feature #160871718] Add refactor test for menu controller

--- api/v2/tests/controllers/menuController.spec.js
+++ api/v2/tests/controllers/menuController.spec.js
@@ -4,69 +4,134 @@ import app from '../../../../server';
 
 const { expect } = chai;
 
+const faketoken = 'qwertyuioplkjjdhdhhdhdhhd';
+
+before((done) => {
+  request(app)
+    .post('/api/v2/auth/login')
+    .send({
+      username: 'testadmin',
+      password: 'testadmin',
+    })
+    .end((err, res) => {
+      global.token = res.body.data.token;
+      done();
+    });
+});
 describe('Test suite for menu controller', () => {
   describe(' POST /api/v2/menu', () => {
-    it('should return succcess status code when a menu is created', (done) => {
+    it('should return error for undefined header and token', (done) => {
       request(app)
-        .post('/api/v2/menu/')
-        .send({
-          foodName: 'Jollof rice and chicken',
-          foodDescr: 'Nigerian jollof with African cusine',
-          price: '$456',
-        })
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .end((err) => {
-          expect(201);
-          if (err) return done(err);
-          done();
-        });
-    });
-    it('should responds with json', (done) => {
-      request(app)
-        .post('/apv2/menu')
+        .post('/api/v2/menu')
         .send({
           foodName: 'Fried rice and chicken',
           foodDescr: 'Nigerian fried with African taste',
           price: '$456',
         })
-        .set('Accept', 'application/json')
-        .expect(200)
-        .end((err) => {
-          if (err) return done(err);
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('Headers key: "Authorization" and "token XXXXXXXXX" should be valid');
           done();
         });
     });
-    it('should be an object with keys and values', (done) => {
+    it('should return error for empty token', (done) => {
       request(app)
-        .post('/api/v1/orders/')
-        .set('Accept', 'application/json')
-        .expect(200)
+        .post('/api/v2/menu')
+        .set({ Authorization: '' })
         .send({
-          foodName: 'spicy chicken burger',
-          foodDescr: 'Nigerian African taste chicken',
+          foodName: 'Jollof rice and chicken',
+          foodDescr: 'Jollof with African taste',
           price: '$456',
         })
         .end((err, res) => {
-          expect(res.err).to.be.not.eql(null);
-          expect(res.status).to.be.not.eql(null);
+          expect(res.status).to.eql(400);
+          expect(res.body.message).to.equal('Headers key: "Authorization" and "token XXXXXXXXX" should not be empty');
           done();
         });
     });
-    it('it return object in json', (done) => {
+    it('should return reject fake token', (done) => {
       request(app)
-        .post('/api/v1/orders')
+        .post('/api/v2/menu')
+        .set({ Authorization: `token ${faketoken}` })
         .send({
-          foodName: 'White rice  and chicken',
-          foodDescr: 'with Nigerian peppered beef',
+          foodName: 'Fried rice and chicken',
+          foodDescr: 'Nigerian fried with African taste',
           price: '$456',
         })
-        .set('Accept', 'application/json')
-        .expect(200)
         .end((err, res) => {
-          expect(res.body.message).to.not.equal(null);
+          expect(res.status).to.eql(401);
+          expect(res.body.auth).to.equal('unauthorized');
+          expect(res.body.message).to.equal('Failed to authenticate token');
+          done();
+        });
+    });
+    it('should return error for empty token', (done) => {
+      request(app)
+        .post('/api/v2/menu')
+        .set({ Authorization: '' })
+        .send({
+          foodName: 'Fried rice and chicken',
+          foodDescr: 'Nigerian fried with African taste',
+          price: '456',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(400);
+          expect(res.body.status).to.equal('operation not successful');
+          expect(res.body.message).to.equal('Headers key: "Authorization" and "token XXXXXXXXX" should not be empty');
+          done();
+        });
+    });
+    it('should return success for new order', (done) => {
+      request(app)
+        .post('/api/v2/menu')
+        .set({ Authorization: `token ${global.token}` })
+        .send({
+          foodName: 'Peppepr soup Chicken',
+          foodDescr: 'Nigerian dishes',
+          price: '456',
+        })
+        .end((err, res) => {
+          expect(res.status).to.eql(201);
+          expect(res.body.status).to.equal('operation successful');
+          expect(res.body.message).to.equal('Menu created successfully');
+          done();
+        });
+    });
+  });
+  describe(' GET /api/v2/menu', () => {
+    it('should return all menu', (done) => {
+      request(app)
+        .get('/api/v2/menu')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.status).to.eql(200);
+          expect(res.body).to.be.an('object');
           expect(res.body.status).to.not.equal(null);
-          expect(res.body.menu).to.not.equal(null);
+          expect(res.body.message).to.not.equal(null);
+          done();
+        });
+    });
+    it('should return all menu', (done) => {
+      request(app)
+        .get('/api/v2/menu')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.status).to.eql(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal('operation successful');
+          expect(res.body.message).to.equal('these are the available menu in our restaurant');
+          done();
+        });
+    });
+    it('should return all menu', (done) => {
+      request(app)
+        .get('/api/v2/menu')
+        .set('Accept', 'application/json')
+        .end((err, res) => {
+          expect(res.status).to.eql(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body).to.have.a.property('menu');
+          expect(res.body.menu).to.be.an('array');
           done();
         });
     });
